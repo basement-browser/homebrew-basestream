c69f5544cdb095798a5b5950816301d841597a4e
Agnel Nieves <agnelnieves@gmail.com>
2026-04-14 12:45:37 -0400

    Update basestream to 0.5.0

--- Formula/basestream.rb
+++ Formula/basestream.rb
@@ -6,11 +6,11 @@ class Basestream < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://90hjjxkaqejv1icr.public.blob.vercel-storage.com/basestream/v0.5.0/basestream-darwin-arm64.tar-8Yd3ePgwapWNSwK5kZDikeqTZgrQPe.gz"
-      sha256 "08bfffd03c1979e2394152a8007fa68fc59d6deab4024b318b834233525e54e5"
+      url "https://90hjjxkaqejv1icr.public.blob.vercel-storage.com/basestream/v0.5.0/basestream-darwin-arm64.tar-JwrjXcwEfkB8RHnwxB9rYcOymSIJM1.gz"
+      sha256 "6fe15c55c6aebcac2eb5b09f920d44f5f086756d502460df35ffa50eced6e9de"
     else
-      url "https://90hjjxkaqejv1icr.public.blob.vercel-storage.com/basestream/v0.5.0/basestream-darwin-x64.tar-DwPNmbI5W3jqxBU9GMO2eAsImFCVt4.gz"
-      sha256 "b5317bdf1f84958b807824dbb38b7f4757e136d056e75f52d8fb6b53d7ad8199"
+      url "https://90hjjxkaqejv1icr.public.blob.vercel-storage.com/basestream/v0.5.0/basestream-darwin-x64.tar-MB5AK8hC77ur4XRsKCVNv5hGYqR5Xt.gz"
+      sha256 "5c4f7d85090ed61b41cccf9ba41ee3134a58d66f4fc50f0b09316d4afe9f30c6"
     end
   end
 
